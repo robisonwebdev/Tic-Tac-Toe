33f83a7f19efc74ed765934a2fe3c81af70ddeb9
David Robison <david@robisonweb.dev>
2021-03-01 08:52:53 -0700

    Got submit button to take both player values and create objects

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
+const Player = (name, gamePiece) => {
+    return {name, gamePiece};
+};
+
+let player1;
+let player2;
+
 const gameboard = (function() {
     let gameboard = {
         row1: [],
@@ -17,7 +24,17 @@ const gameboard = (function() {
 
 const gameControls = (function() {
     const gameSquare = document.querySelectorAll('.gameSquare');
+    const submitBtn = document.querySelector('#submitBtn');
+
     let tempVar = 'X';
+
+    function createPlayers() {
+        const playerOneValue = document.querySelector('#playerOne').value;
+        const playerTwoValue = document.querySelector('#playerTwo').value;
+        
+        player1 = Player(playerOneValue, 'X');
+        player2 = Player(playerTwoValue, 'O');        
+    }
 
     gameSquare.forEach((square) => {
         square.addEventListener('click', (e) => {
@@ -31,7 +48,21 @@ const gameControls = (function() {
             gameboard.updateSquare(e.target.dataset.row, e.target.dataset.square, tempVar);
         })
     })
+
+    submitBtn.addEventListener('click', () => {
+        console.log('click');
+        createPlayers();
+    })
+
+    return {
+        
+    }
 })();
+
+    
+
+
+
 
 // Saved for later
 // e.target.dataset.row
